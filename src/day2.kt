2c2634f44241dfe3843a533bda8fc6bf3092bea8
Simon Weller <s.meierbeck@gmail.com>
2019-11-09 13:16:30 +0100

    day2 - extracted abstract class Keypad

--- src/day2.kt
+++ src/day2.kt
@@ -6,67 +6,23 @@ fun main() {
     println(BathroomKeypad('5').getCode(directions))
 }
 
-class BathroomKeypad(key: Char) {
-    private var keys = mutableListOf(
-        mutableListOf(null, null, '1', null, null),
-        mutableListOf(null, '2', '3', '4', null),
-        mutableListOf('5', '6', '7', '8', '9'),
-        mutableListOf(null, 'A', 'B', 'C', null),
-        mutableListOf(null, null, 'D', null, null)
-    )
-
+abstract class Keypad {
+    protected abstract var keys: MutableList<MutableList<out Char?>>
+    protected abstract var rowIndex: Int
+    protected abstract var colIndex: Int
     private val key: Char?
     get() = keys[rowIndex][colIndex]
 
-    private var rowIndex = keys.indexOfFirst { it.contains(key) }
-    private var colIndex = keys[rowIndex].indexOfFirst { it == key }
-
     fun getCode(directions: List<List<Direction>>): String {
         var code = ""
         directions.forEach {
-            move(it)
+            goToKey(it)
             code += key
         }
         return code
     }
 
-    private fun move(directions: List<Direction>) {
-        directions.forEach { move(it) }
-    }
-
-    private fun move(direction: Direction) {
-        when {
-            direction == Direction.U && keys.getOrNull(rowIndex - 1)?.getOrNull(colIndex) != null -> rowIndex--
-            direction == Direction.D && keys.getOrNull(rowIndex + 1)?.getOrNull(colIndex) != null -> rowIndex++
-            direction == Direction.L && keys.getOrNull(rowIndex)?.getOrNull(colIndex - 1) != null -> colIndex--
-            direction == Direction.R && keys.getOrNull(rowIndex)?.getOrNull(colIndex + 1) != null -> colIndex++
-        }
-    }
-}
-
-class RegularKeypad(key: Char) {
-    private var keys = mutableListOf(
-        mutableListOf('1', '2', '3'),
-        mutableListOf('4', '5', '6'),
-        mutableListOf('7', '8', '9')
-    )
-
-    private val key: Char
-    get() = keys[rowIndex][colIndex]
-
-    private var rowIndex = keys.indexOfFirst { it.contains(key) }
-    private var colIndex = keys[rowIndex].indexOfFirst { it == key }
-
-    fun getCode(directions: List<List<Direction>>): String {
-        var code = ""
-        directions.forEach {
-            move(it)
-            code += key
-        }
-        return code
-    }
-
-    private fun move(directions: List<Direction>) {
+    private fun goToKey(directions: List<Direction>) {
         directions.forEach { move(it) }
     }
 
@@ -85,4 +41,28 @@ enum class Direction {
     D,
     L,
     R
+}
+
+class BathroomKeypad(initialKey: Char): Keypad() {
+    override var keys = mutableListOf(
+        mutableListOf(null, null, '1', null, null),
+        mutableListOf(null, '2', '3', '4', null),
+        mutableListOf('5', '6', '7', '8', '9'),
+        mutableListOf(null, 'A', 'B', 'C', null),
+        mutableListOf(null, null, 'D', null, null)
+    )
+
+    override var rowIndex = keys.indexOfFirst { it.contains(initialKey) }
+    override var colIndex = keys[rowIndex].indexOfFirst { it == initialKey }
+}
+
+class RegularKeypad(initialKey: Char): Keypad() {
+    override var keys: MutableList<MutableList<out Char?>> = mutableListOf(
+        mutableListOf('1', '2', '3'),
+        mutableListOf('4', '5', '6'),
+        mutableListOf('7', '8', '9')
+    )
+
+    override var rowIndex = keys.indexOfFirst { it.contains(initialKey) }
+    override var colIndex = keys[rowIndex].indexOfFirst { it == initialKey }
 }
