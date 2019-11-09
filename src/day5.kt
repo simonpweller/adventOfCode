4bb4c09fd431437628d2793022473d77971a0f3f
Simon Weller <s.meierbeck@gmail.com>
2019-11-09 22:19:48 +0100

    day 5 cleaned up

--- src/day5.kt
+++ src/day5.kt
@@ -4,38 +4,28 @@ import java.security.MessageDigest
 
 fun main() {
     val input = File("src/day5.txt").readText()
-    println(getFirstPassword(input))
-    println(getSecondPassword(input))
+    println(getPassword(input, ::passwordModifierPart1))
+    println(getPassword(input, ::passwordModifierPart2))
 }
 
-
-fun getFirstPassword(input: String): String {
-    val md5 = MessageDigest.getInstance("MD5")
-    var password = ""
-    var index = 0
-    while(password.length < 8) {
-        val hash = String.format("%032x", BigInteger(1, md5.digest("$input$index".toByteArray())))
-        if (hash.startsWith("00000")) password += hash[5]
-        index++
-    }
-    return password
-}
-
-fun getSecondPassword(input: String): String {
+fun getPassword(input: String, passwordModifier: (String, String) -> String): String {
     val md5 = MessageDigest.getInstance("MD5")
     var password = "________"
     var index = 0
     while(password.contains('_')) {
         val hash = String.format("%032x", BigInteger(1, md5.digest("$input$index".toByteArray())))
-        if (hash.startsWith("00000")) {
-            if (hash[5] in '0' until '8') {
-                val position = hash[5].toString().toInt()
-                if (password[position] == '_') {
-                    password = password.replaceRange(position, position + 1, hash[6].toString())
-                }
-            }
-        }
+        if (hash.startsWith("00000")) password = passwordModifier(password, hash)
         index++
     }
     return password
+}
+
+private fun passwordModifierPart1(password: String, hash: String): String = password.replaceFirst('_', hash[5])
+
+private fun passwordModifierPart2(password: String, hash: String): String {
+    val value = hash.substring(6, 7)
+    val position = hash[5].toString().toIntOrNull()
+    return if (position is Int && position in 0 until 8 && password[position] == '_')
+        password.replaceRange(position, position + 1, value)
+        else password
 }
