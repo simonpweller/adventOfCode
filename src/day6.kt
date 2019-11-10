6cc4497ff9aa569fb4a8337a46a286112280df6a
Simon Weller <s.meierbeck@gmail.com>
2019-11-10 12:49:28 +0100

    day 6

---
+++ src/day6.kt
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun main() {
+    val lines = File("src/day6.txt").readLines()
+    println(correctErrors(lines, ::maxCharCount))
+    println(correctErrors(lines, ::minCharCount))
+}
+
+fun correctErrors(lines: List<String>, heuristic: (List<Pair<Char, Int>>) -> Char?): String {
+    val listOfCharCountsAtIndices = lines.fold(List(lines.first().length) { mapOf<Char, Int>() }) { list, line ->
+        list.mapIndexed { index, map ->
+            map.plus(Pair(line[index], map.getOrDefault(line[index], 0) + 1))
+        }
+    }
+    return listOfCharCountsAtIndices.map { it.toList() }.map { heuristic(it) }.joinToString("")
+}
+
+fun minCharCount(list: List<Pair<Char, Int>>): Char? = list.minBy { it.second }?.first
+fun maxCharCount(list: List<Pair<Char, Int>>): Char? = list.maxBy { it.second }?.first
