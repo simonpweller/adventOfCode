d17a16964ce91312f47f6b3e2821200ef224e3d9
Simon Weller <s.meierbeck@gmail.com>
2019-11-23 18:00:51 +0100

    2017 - day9

---
+++ src/y2017/day9/day9.kt
@@ -0,0 +1,29 @@
+package y2017.day9
+
+import java.io.File
+
+fun main() {
+    val input = File("src/y2017/day9/input.txt").readText()
+    val garbageRegex = "<[^>]*>".toRegex()
+    val escapedInput = input.replace("!.".toRegex(), "")
+    println(groupCount(escapedInput.replace(garbageRegex, "").replace(",", "")))
+    println(garbageRegex.findAll(escapedInput).map { it.value.length - 2 }.sum())
+}
+
+fun groupCount(group: String, outerValue: Int = 1): Int =
+    outerValue + innerGroups(group).map { groupCount(it, outerValue = outerValue + 1) }.sum()
+
+fun innerGroups(group: String): List<String> {
+    val groups = mutableListOf<String>()
+    var currentGroup = ""
+    var openBrackets = 0
+    group.subSequence(1, group.lastIndex).forEach {
+        currentGroup += it
+        openBrackets += if (it == '{') 1 else -1
+        if (openBrackets == 0) {
+            groups.add(currentGroup)
+            currentGroup = ""
+        }
+    }
+    return groups
+}
