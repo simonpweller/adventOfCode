1c11eff9c2ca22fe9910dfad1bd1839eea3df73e
Simon Weller <s.meierbeck@gmail.com>
2019-11-24 21:33:07 +0100

    2016 - day14 - part1, optimized

--- src/y2016/day14/day14.kt
+++ src/y2016/day14/day14.kt
@@ -9,25 +9,22 @@ fun main() {
     println(generateKeys(input).take(64).last())
 }
 
+class Hashes(private val input: String) {
+    private val map = mutableMapOf<Int, String>()
+    private val md5 = MessageDigest.getInstance("MD5")
+
+    fun get(index: Int) =
+        map.getOrPut(index) {String.format("%032x", BigInteger(1, md5.digest("$input$index".toByteArray())))}
+}
+
 fun generateKeys(input: String) = sequence {
-    val md5 = MessageDigest.getInstance("MD5")
-    yieldAll(generateSequence(0) { it + 1 }
-        .filter {index ->
-            isKey(String.format("%032x", BigInteger(1, md5.digest("$input$index".toByteArray()))), input, index)
-        })
+    val hashes = Hashes(input)
+    yieldAll(generateSequence(0) { it + 1 }.filter { index -> isKey(hashes, index)})
 }
 
-fun isKey(hash: String, input: String, index: Int): Boolean {
-    val tripletChar =  getTripletChar(hash)
-    return if (tripletChar == null) false else hasQuintupletInNext1000Hashes(tripletChar, input, index)
-}
-
-fun hasQuintupletInNext1000Hashes(tripletChar: Char, input: String, index: Int): Boolean {
-    val md5 = MessageDigest.getInstance("MD5")
-    return (index + 1 .. index + 1000)
-        .any { newIndex ->
-            String.format("%032x", BigInteger(1, md5.digest("$input$newIndex".toByteArray()))).contains(tripletChar.toString().repeat(5))
-        }
+private fun isKey(hashes: Hashes, index: Int): Boolean {
+    val searchString = getTripletChar(hashes.get(index))?.toString()?.repeat(5)
+    return if (searchString == null) false else (index + 1..index + 1000).any { hashes.get(it).contains(searchString) }
 }
 
 fun getTripletChar(hash: String): Char? = "([a-z\\d])\\1{2,}".toRegex().find(hash)?.value?.first()
