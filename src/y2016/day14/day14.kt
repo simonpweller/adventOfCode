dc8a7786fb38c5ea8cb0a76ffdc4935ec5139b46
Simon Weller <s.meierbeck@gmail.com>
2019-11-24 21:46:19 +0100

    2016 - day14 - part2

--- src/y2016/day14/day14.kt
+++ src/y2016/day14/day14.kt
@@ -7,18 +7,25 @@ import java.security.MessageDigest
 fun main() {
     val input = File("src/y2016/day14/input.txt").readText()
     println(generateKeys(input).take(64).last())
+    println(generateKeys(input, true).take(64).last())
 }
 
-class Hashes(private val input: String) {
+class Hashes(private val input: String, private val stretch: Boolean) {
     private val map = mutableMapOf<Int, String>()
     private val md5 = MessageDigest.getInstance("MD5")
 
     fun get(index: Int) =
-        map.getOrPut(index) {String.format("%032x", BigInteger(1, md5.digest("$input$index".toByteArray())))}
+        map.getOrPut(index) {
+            var hash = String.format("%032x", BigInteger(1, md5.digest("$input$index".toByteArray())))
+            if (stretch) {
+                repeat(2016) { hash = String.format("%032x", BigInteger(1, md5.digest(hash.toByteArray())))}
+            }
+            hash
+        }
 }
 
-fun generateKeys(input: String) = sequence {
-    val hashes = Hashes(input)
+fun generateKeys(input: String, stretch: Boolean = false) = sequence {
+    val hashes = Hashes(input, stretch)
     yieldAll(generateSequence(0) { it + 1 }.filter { index -> isKey(hashes, index)})
 }
 
