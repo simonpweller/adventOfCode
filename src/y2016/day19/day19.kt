af5b3fbb233a122badc8727a89a904feb4957a0a
Simon Weller <s.meierbeck@gmail.com>
2019-11-27 23:55:34 +0100

    2016 - day19 - part 1, accelerated with custom CircularLinkedList

--- src/y2016/day19/day19.kt
+++ src/y2016/day19/day19.kt
@@ -1,32 +1,41 @@
 package y2016.day19
 
-import InfiniteList
 import java.io.File
+import java.lang.IllegalArgumentException
 
 fun main() {
     val numberOfElves = File("src/y2016/day19/input.txt").readText().toInt()
     println(Circle(numberOfElves).getWinningElf())
 }
 
-class Circle(private val numberOfElves: Int) {
-    private val presentCounts = InfiniteList((1 .. numberOfElves).map { Elf(it) }.toMutableList())
+class Circle(numberOfElves: Int) {
+    private val presentCounts = CircularLinkedList((1 .. numberOfElves).toList())
 
     fun getWinningElf(): Int {
-        var index = 0
+        var elf = presentCounts.start
         do {
-            if (presentCounts[index].presents != 0) {
-                val indexToLeft = getIndexToLeft(index)
-                val presentsToSteal = presentCounts[indexToLeft].presents
-                presentCounts[index].presents += presentsToSteal
-                presentCounts[indexToLeft].presents = 0
-            }
-            index++
-        } while (presentCounts[index].presents < numberOfElves)
-        return presentCounts[index].number
+            elf.removeNext()
+            elf = elf.next
+        } while (elf.next != elf)
+        return elf.value
     }
-
-    private fun getIndexToLeft(index: Int): Int =
-        generateSequence(index + 1) { it + 1}.filter { ix -> presentCounts[ix].presents != 0 }.first()
 }
 
-class Elf(val number: Int, var presents: Int = 1)
+class CircularLinkedList<Int>(list: List<Int>) {
+
+    var start = (list.lastIndex downTo 0).fold(null) {last: Node?, index -> Node(list[index], last)}
+        ?: throw IllegalArgumentException("linked list may not be empty")
+
+    inner class Node(val value: Int, var link: Node? = null) {
+        val next: Node
+            get() = link ?: start
+
+        fun removeNext() {
+            val newLink = next.next
+            if (next == start) {
+                start = newLink
+            }
+            link = newLink
+        }
+    }
+}
