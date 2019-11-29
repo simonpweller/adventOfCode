a7422a083404e4fd7c1851a790d1f9aa220587ae
Simon Weller <s.meierbeck@gmail.com>
2019-11-29 23:21:52 +0100

    2016 - day20 - fast solution

--- src/y2016/day20/day20.kt
+++ src/y2016/day20/day20.kt
@@ -1,12 +1,30 @@
 package y2016.day20
 
 import java.io.File
+import java.lang.Long.max
 
 fun main() {
+    val upperLimitExclusive = 4294967296L
+
     val blockedRanges = File("src/y2016/day20/input.txt").readLines()
         .map { range -> range.split("-").map { it.toLong() } }
         .map { LongRange(it.first(), it.last()) }
+        .sortedBy { it.first }
 
-    println(generateSequence(0) { it + 1}.first {blockedRanges.none { range -> range.contains(it) }})
-    println((0 .. 4294967295).count {blockedRanges.none { range -> range.contains(it) }})
+    val reducedRanges = blockedRanges.plusElement(LongRange(upperLimitExclusive, upperLimitExclusive))
+        .fold(listOf()) { acc: List<LongRange>, curr: LongRange ->
+            val prev = acc.lastOrNull() ?: LongRange(0, 0)
+            if (prev.last >= curr.first) {
+                acc.dropLast(1).plusElement(LongRange(prev.first, max(prev.last, curr.last)))
+            } else {
+                acc.plusElement(curr)
+            }
+    }
+
+    println(generateSequence(0) { it + 1}.first {reducedRanges.none { range -> range.contains(it) }})
+    println(reducedRanges.fold(RunningResult(0L, 0L)) { runningResult, blockedRange ->
+        RunningResult(blockedRange.last + 1, runningResult.ipCount + max(0, blockedRange.first - runningResult.next))
+    }.ipCount)
 }
+
+class RunningResult(val next: Long, val ipCount: Long)
