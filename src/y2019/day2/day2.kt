43af5db567cc243933a6ac140fc7d6bfdc568c7d
Simon Weller <s.meierbeck@gmail.com>
2019-12-02 21:52:33 +0100

    2019 - day2 cleaned up a bit

--- src/y2019/day2/day2.kt
+++ src/y2019/day2/day2.kt
@@ -1,58 +1,57 @@
 package y2019.day2
 
 import java.io.File
-import kotlin.system.exitProcess
+import java.lang.IllegalArgumentException
+import java.lang.RuntimeException
 
 fun main() {
     val program = File("src/y2019/day2/input.txt").readText().split(",").map { it.toInt() }.toIntArray()
-    println(testInitialValues(program, 12, 2))
+    println(IntComputer(program, 12, 2).run())
+    println(part2(program))
+}
+
+fun part2(program: IntArray): Int {
     (0 .. 99).forEach { noun ->
         (0 .. 99).forEach { verb ->
-            if (testInitialValues(program, noun, verb) == 19690720) {
-                println(100 * noun + verb)
-                exitProcess(0)
-            }
+                if (IntComputer(program, noun, verb).run() == 19690720) return 100 * noun + verb
         }
     }
+    throw RuntimeException("No combination of noun and verb found that returns the correct value")
 }
 
-fun testInitialValues(program: IntArray, noun: Int, verb: Int): Int {
-    val programToTest = program.clone()
-    programToTest[1] = noun
-    programToTest[2] = verb
-    val intComputer = IntComputer(programToTest)
-    intComputer.run()
-    return intComputer.program[0]
-}
-
-class IntComputer(val program: IntArray) {
-    var programCounter = 0
-    private val opcode: Int
-        get() = program[programCounter]
-    private val operand1: Int
-        get() = program[programCounter + 1]
-    private val operand2: Int
-        get() = program[programCounter + 2]
-    private val target: Int
-        get() = program[programCounter + 3]
+class IntComputer(program: IntArray, noun: Int, verb: Int) {
+    private val memory = program.clone().apply { set(1, noun) }.apply { set(2, verb) }
+    private var programCounter = 0
 
     fun run(): Int {
-        while (true) {
+        while (opcode != 99) {
             when(opcode) {
                 1 -> add()
                 2 -> multiply()
-                99 -> return 0
-                else -> return -1
+                else -> throw IllegalArgumentException("opcode $opcode is not supported")
             }
             programCounter += 4
         }
+        return memory[0]
     }
 
     private fun multiply() {
-        program[target] = program[operand1] * program[operand2]
+        target = operand1 * operand2
     }
 
     private fun add() {
-        program[target] = program[operand1] + program[operand2]
+        target = operand1 + operand2
     }
+
+    private val opcode: Int
+        get() = memory[programCounter]
+    private val operand1: Int
+        get() = memory[memory[programCounter + 1]]
+    private val operand2: Int
+        get() = memory[memory[programCounter + 2]]
+    private var target
+        get() = memory[memory[programCounter + 3]]
+        set(value) {
+            memory[memory[programCounter + 3]] = value
+        }
 }
