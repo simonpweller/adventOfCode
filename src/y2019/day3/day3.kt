4c57756679f7b884b184b4f327771441b31b4292
Simon Weller <s.meierbeck@gmail.com>
2019-12-03 07:51:28 +0100

    2019 - day3 (part 1)

--- src/y2019/day3/day3.kt
+++ src/y2019/day3/day3.kt
@@ -4,4 +4,37 @@ import java.io.File
 
 fun main() {
     val lines = File("src/y2019/day3/input.txt").readLines()
+    val firstWirePoints = visitedPoints(lines[0])
+    val secondWirePoints = visitedPoints(lines[1])
+    println(firstWirePoints.intersect(secondWirePoints))
+}
+
+private fun visitedPoints(wire: String): Set<Pair<Int, Int>> {
+    var x = 0
+    var y = 0
+    return wire.split(",").fold(setOf()) { acc, curr ->
+        val steps = curr.drop(1).toInt()
+        when (curr[0]) {
+            'R' -> {
+                val newAcc = acc.plus((1..steps).map { Pair(x + it, y) })
+                x += steps
+                newAcc
+            }
+            'D' -> {
+                val newAcc = acc.plus((1..steps).map { Pair(x, y - it) })
+                y -= steps
+                newAcc
+            }
+            'L' -> {
+                val newAcc = acc.plus((1..steps).map { Pair(x - it, y) })
+                x -= steps
+                newAcc
+            }
+            else -> {
+                val newAcc = acc.plus((1..steps).map { Pair(x, y + it) })
+                y += steps
+                newAcc
+            }
+        }
+    }
 }
