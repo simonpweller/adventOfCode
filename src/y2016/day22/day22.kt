1bc9bd1f54f9ae004cf8f8d2875a62ddff3285aa
Simon Weller <s.meierbeck@gmail.com>
2019-12-04 23:30:32 +0100

    2016 - day22 (shifted dataLocation to game state)

--- src/y2016/day22/day22.kt
+++ src/y2016/day22/day22.kt
@@ -11,11 +11,13 @@ fun main() {
 }
 
 private fun part2(nodeLines: List<String>): Int {
-    var gameStates = listOf(GameState(createNodeMap(nodeLines), 0))
-    while (gameStates.none { it.nodeMap[Point(0, 0)]!!.holdsData }) {
+    val nodeMap = createNodeMap(nodeLines)
+    val maxX = nodeMap.keys.map { it.x }.max() ?: error("no max for x found")
+    var gameStates = listOf(GameState(nodeMap, 0, Point(maxX, 0)))
+    while (gameStates.none { it.dataLocation == Point(0, 0) }) {
         gameStates = gameStates.flatMap { gameState -> getNextStates(gameState) }
     }
-    return gameStates.find { it.nodeMap[Point(0, 0)]!!.holdsData }!!.moves
+    return gameStates.find { it.dataLocation == Point(0, 0) }!!.moves
 }
 
 private fun part1(nodeLines: List<String>) =
@@ -26,23 +28,20 @@ private fun getNextStates(gameState: GameState): List<GameState> {
     return findMoves(nodeMap).map {move ->
         val dest = nodeMap[move.to] ?: error("destination does not exist")
         val source = nodeMap[move.from] ?: error("source does not exist")
-        val newDest = Node(dest.used + source.used, dest.available - source.used, source.holdsData)
-        val newSource = Node(0, source.available + source.used, false)
-        GameState(nodeMap.plus(Pair(move.to, newDest)).plus(Pair(move.from, newSource)), gameState.moves + 1)
+        val newDest = Node(dest.used + source.used, dest.available - source.used)
+        val newSource = Node(0, source.available + source.used)
+        val dataLocation = if (move.from == gameState.dataLocation) move.to else gameState.dataLocation
+        GameState(nodeMap.plus(Pair(move.to, newDest)).plus(Pair(move.from, newSource)), gameState.moves + 1, dataLocation)
     }
 }
 
-private fun createNodeMap(nodeLines: List<String>): Map<Point, Node> {
-    val nodeMap = nodeLines.fold<String, Map<Point, Node>>(mapOf()) { map, nodeLine ->
+private fun createNodeMap(nodeLines: List<String>): Map<Point, Node> =
+    nodeLines.fold(mapOf()) { map, nodeLine ->
         val point = Point(
             nodeLine.substringAfter("x").substringBefore("-").toInt(),
             nodeLine.substringAfter("y").substringBefore(" ").toInt()
         )
         map.plus(Pair(point, Node(nodeLine)))
-    }
-    val maxX = nodeMap.keys.map { it.x }.max() ?: error("no max for x found")
-    nodeMap.filterKeys { point -> point == Point(maxX, 0)}.entries.first().value.holdsData = true
-    return nodeMap
 }
 
 private fun findMoves(nodeMap: Map<Point, Node>): List<Move> =
@@ -52,7 +51,7 @@ private fun findMoves(nodeMap: Map<Point, Node>): List<Move> =
             .filter { (nodeMap[it.to] ?: return@filter false).available > node.used }
     }
 
-private data class Node(val used: Int, val available: Int, var holdsData: Boolean = false) {
+private data class Node(val used: Int, val available: Int) {
     constructor(node: String): this(
         node.substring(28).substringBefore("T").trim().toInt(),
         node.substring(35).substringBefore("T").trim().toInt()
@@ -60,4 +59,4 @@ private data class Node(val used: Int, val available: Int, var holdsData: Boolea
 }
 
 private data class Move(val from: Point, val to: Point)
-private data class GameState(val nodeMap: Map<Point, Node>, val moves: Int)
+private data class GameState(val nodeMap: Map<Point, Node>, val moves: Int, val dataLocation: Point)
