3d11005866f633b616a10dfdc34de09212be1148
Simon Weller <s.meierbeck@gmail.com>
2019-12-06 23:23:31 +0100

    2018 - day2

---
+++ src/y2018/day2/day2.kt
@@ -0,0 +1,24 @@
+package y2018.day2
+import resourceLines
+
+fun main() {
+    val ids = resourceLines(2018, 2)
+    println(part1(ids))
+    println(part2(ids))
+}
+
+private fun part1(ids: List<String>): Int =
+    ids.fold(Pair(0, 0)) { counts, id ->
+        Pair(
+            counts.first + if (id.groupBy { it }.values.any { it.size == 2 }) 1 else 0,
+            counts.second + if (id.groupBy { it }.values.any { it.size == 3 }) 1 else 0
+        )
+    }.let { it.first * it.second }
+
+private fun part2(ids: List<String>): String =
+    ids.flatMap { id1 ->
+        ids.mapNotNull { id2 ->
+            val sharedChars = id1.indices.mapNotNull { idx -> if (id1[idx] == id2[idx]) id1[idx] else null }
+            if (sharedChars.size == id1.length - 1) sharedChars.joinToString("") else null
+        }
+    }.first()
