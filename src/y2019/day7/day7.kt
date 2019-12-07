77ef7649c25de02c2292cf7ea7ece569281c6097
Simon Weller <s.meierbeck@gmail.com>
2019-12-07 18:45:05 +0100

    2019 - day7 (part 2) - cleaned up a bit

--- src/y2019/day7/day7.kt
+++ src/y2019/day7/day7.kt
@@ -10,20 +10,19 @@ fun main() {
     println(permutations((5 .. 9).toSet()).map { part2(it, program) }.max())
 }
 
-private fun part1(config: List<Int>, program: IntArray): Int =
-    config.fold(0) { acc, curr ->
-        IntComputer(program).addInput(curr).addInput(acc).run().outputs.first()
+private fun part1(config: List<Int>, program: IntArray): Int {
+    val amplifiers = config.map { IntComputer(program).addInput(it) }
+    return amplifiers.fold(0) { acc, curr ->
+        curr.addInput(acc).run().outputs.first()
     }
+}
 
 private fun part2(config: List<Int>, program: IntArray): Int {
     val amplifiers = config.map { IntComputer(program).addInput(it) }
     var output = listOf(0)
     while (!amplifiers.last().isDone) {
         output = amplifiers.fold(output) { acc, curr ->
-            val comp = curr.addInput(acc)
-            val out = comp.run().outputs
-            curr.outputs = mutableListOf()
-            out
+            curr.addInput(acc).run().outputs.also { curr.outputs = mutableListOf() }
         }
     }
     return output.first()
