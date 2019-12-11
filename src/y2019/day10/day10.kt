515ac51a7ca6045af2eb67f696d7c061974d605c
Simon Weller <s.meierbeck@gmail.com>
2019-12-11 01:00:18 +0100

    day 10 - part 2

--- src/y2019/day10/day10.kt
+++ src/y2019/day10/day10.kt
@@ -1,21 +1,61 @@
 package y2019.day10
 
 import Point
+import Vector
 import resourceLines
+import kotlin.math.sign
 
 fun main() {
     val asteroids = resourceLines(2019, 10).mapIndexed { y, line ->
         line.mapIndexed { x, ch -> if (ch == '#') Point(x, y) else null }
     }.flatten().filterNotNull()
 
-    println(part1(asteroids))
+    val (placement, reachableAsteroids) = bestPlacement(asteroids) ?: error("no optimal placement found")
+    println(reachableAsteroids)
+    println(asteroid200(asteroids, placement).run { x * 100 + y })
 }
 
-private fun part1(asteroids: List<Point>): Int? =
-    asteroids.map { location ->
+private fun asteroid200(asteroids: List<Point>, placement: Point): Point {
+    val destructionVectors = destructionVectors(asteroids, placement).toMutableMap()
+    var destroyed = 0
+    while (true) {
+        destructionVectors.entries.filter { it.value.isNotEmpty() }.forEach { entry ->
+            val asteroidToDestroy = entry.value.minBy { it.manhattan(placement) } ?: error("nothing to destroy!")
+            destructionVectors[entry.key] = entry.value.minus(asteroidToDestroy)
+            destroyed++
+            if (destroyed == 200) return asteroidToDestroy
+        }
+    }
+}
+
+private fun bestPlacement(asteroids: List<Point>) = asteroids.map { location ->
+    Pair(
+        location,
         asteroids
             .filter { it != location }
             .map { location - it }
             .map { it.reduce() }
             .toSet().size
-    }.max()
+    )}.maxBy { it.second }
+
+private fun destructionVectors(asteroids: List<Point>, location: Point): Map<Vector, List<Point>> {
+    return asteroids.filter { it != location }.groupBy { (it - location).reduce() }.toSortedMap(object: Comparator<Vector> {
+        override fun compare(o1: Vector?, o2: Vector?): Int {
+            if(o1 == null || o2 == null) throw IllegalArgumentException("cannot compare null vector")
+            if (quadrant(o1) != quadrant(o2)) return (quadrant(o1) - quadrant(o2)).sign
+            return ((o1.y.toDouble() / o1.x.toDouble()) - (o2.y.toDouble() / o2.x.toDouble())).sign.toInt()
+        }
+
+        private fun quadrant(vector: Vector): Int = when {
+            vector.x == 0 && vector.y < 0 -> 1
+            vector.x > 0 && vector.y < 0 -> 2
+            vector.x > 0 && vector.y == 0 -> 3
+            vector.x > 0 && vector.y > 0 -> 4
+            vector.x == 0 && vector.y > 0 -> 5
+            vector.x < 0 && vector.y > 0 -> 6
+            vector.x < 0 && vector.y == 0 -> 7
+            vector.x < 0 && vector.y < 0 -> 8
+            else -> throw IllegalStateException("missed an option")
+        }
+    })
+}
