3a6388f48e71d25231b49d9c650eb440ed928234
Simon Weller <s.meierbeck@gmail.com>
2019-12-17 00:35:37 +0100

    2019 - day 16 - part 1 optimized a bit

--- src/y2019/day16/day16.kt
+++ src/y2019/day16/day16.kt
@@ -4,20 +4,22 @@ import resourceText
 
 fun main() {
     val input = resourceText(2019, 16)
-    println(FFT(input).transform(100).subList(0, 8).joinToString(""))
+    println(part1(input))
 }
+
+private fun part1(input: String) = FFT(input).transform(100).subList(0, 8).joinToString("")
 
 class FFT(val input: String) {
     private val basePattern = listOf(0, 1, 0, -1)
-
-    private fun pattern(reps: Int, size: Int): List<Int> =
-        basePattern.flatMap { sequenceOf(it).repeat().take(reps).toList() }.asSequence().repeat().drop(1).take(size).toList()
+    private val multiplicationMaps = (1 .. input.length).map { reps ->
+        basePattern.flatMap { sequenceOf(it).repeat().take(reps).toList() }.asSequence().repeat().drop(1).take(input.length).toList()
+    }
 
     fun transform(phases: Int): List<Int> {
         var output = input.map { it.toString().toInt() }
         repeat(phases) {
-            output = (0 .. input.lastIndex).map { index ->
-                output.zip(pattern(index + 1, input.length)) { a: Int, b: Int -> a * b}.sum().toString().takeLast(1).toInt()
+            output = (0 .. input.lastIndex).map { digit ->
+                output.foldIndexed(0) {index, acc, i -> acc + (i * multiplicationMaps[digit][index]) }.toString().takeLast(1).toInt()
             }
         }
         return output
